267cec9294cd989ed62e10b0af245978c0d78661
chan <nrg5477@naver.com>
2024-09-09 20:22:15 +0900

    Initial commit

---
+++ src/test/java/org/example/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
